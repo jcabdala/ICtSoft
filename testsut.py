9b09e0c06a49a14eb6bad1faa9e60f1dd22e05ad
Juan Abdala <abdalajc@gmail.com>
2016-06-30 15:20:39 -0300

    first commit

---
+++ testsut.py
@@ -0,0 +1,36 @@
+import unittest
+import sut
+from unittest.mock import MagicMock
+from unittest.mock import patch
+#import math
+
+class TestSut(unittest.TestCase):
+
+    def tests_area(self):
+        area = sut.area(3,2)
+        self.assertTrue(area==6)
+    
+    def test_costototal(self):
+        sut.sumar=MagicMock(return_value=2)
+        a = sut.costototal(3,2)
+        print(a)
+        self.assertTrue(a == "El costo total es $2")
+
+
+    def test_supercalc(self):
+    	math.exp=MagicMock(return_value=2)
+    	math.sqrt=MagicMock(return_value=2)
+    	a = sut.supercalc(3)
+    	self.assertTrue(a == 2)
+	
+    @patch('math.exp')
+    @patch('math.sqrt')
+    def test_supercalc(self, sqrt, exp):
+    	sqrt.return_value = 2
+    	exp.return_value = 2
+    	a = sut.supercalc(3)
+    	self.assertTrue(a == 2)
+
+        
+if __name__ == '__main__':
+    unittest.main()
